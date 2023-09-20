48f27c6ec1a554b165f7cfe58a1f9efa649cb802
Maria Isabela Silva Nunes <mariaisabelasn@gmail.com>
2023-09-20 14:41:05 -0300

    .

--- Campeonato.java
+++ Campeonato.java
@@ -1,5 +1,4 @@
 package JogoGeneralTrabalho;
-import jaava.util.Scanner;
 
 public class Campeonato {
     private Jogador[] players = new Jogador[5];
--- UsaCampeonato.java
+++ UsaCampeonato.java
@@ -6,31 +6,31 @@ public class Campeonato {
        
     public stat ic void main(String[] args){
 
-    Scanner teclado = new Scanner (System.in);
-    int opcao = 0;
+        Scanner teclado = new Scanner (System.in);
+        int opcao = 0;
 
-    do{
-        System.out.println ("..:: Menu interativo ::..");
-        System.out.println ("1 - Ola mundo");
-        System.out.println ("2 - Ola POO" );
-        System.out.println ("3 - Sair");
-        System.out.print ("Entre com uma opcao: ");
-        opcao = teclado.nextInt( );
+        do{
+            System.out.println ("..:: Menu interativo ::..");
+            System.out.println ("1 - Ola mundo");
+            System.out.println ("2 - Ola POO" );
+            System.out.println ("3 - Sair");
+            System.out.print ("Entre com uma opcao: ");
+            opcao = teclado.nextInt( );
 
-        switch( opcao ){
-            case 1:
-                System.out.println ("Ola mundo");
-                break;
-            case 2:
-                System.out.println ("Ola POO");
-                break;
-            case 3:
-                System.out.println ("Saindo");
-                break;
-            default :
-                System.out.println ("Opcao invalida. Tente novamente");
-        }
-    }while(opcao != 3);
+            switch( opcao ){
+                case 1:
+                    System.out.println ("Ola mundo");
+                    break;
+                case 2:
+                    System.out.println ("Ola POO");
+                    break;
+                case 3:
+                    System.out.println ("Saindo");
+                    break;
+                default :
+                    System.out.println ("Opcao invalida. Tente novamente");
+            }
+        }while(opcao != 3);
 
     }
  }

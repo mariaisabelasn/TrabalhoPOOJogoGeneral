22f0ed0121fc439c821884e6fba9adc77afd4bbc
MacriFabiane <mariacristinafabiane@gmail.com>
2023-09-20 14:50:34 -0300

    validajogada

--- JogoGeneral.java
+++ JogoGeneral.java
@@ -23,18 +23,65 @@ public class JogoGeneral {
         return dados;
     }
 
-    public Boolean validarJogada(int njogada, Dado[] dados){ //vai verificar se a jogada escolhida é valida retorna 1 vdd 0 falso
+    public Boolean validarJogada(String njogada, Dado[] dados){ //vai verificar se a jogada escolhida é valida retorna 1 vdd 0 falso
         int i=0;
-    if(njogada==1){
+    if(njogada=="1"){
         do{
             if(dados[i]==1){
-                break;
+                return 1; //certo
             }
 
-        }while(i!=5)
-        teste
+        }while(i!=5);
     }
-    
+    if(njogada=="2"){
+        do{
+            if(dados[i]==2){
+               return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+    if(njogada=="3"){
+        do{
+            if(dados[i]==3){
+                return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+    if(njogada=="4"){
+        do{
+            if(dados[i]==4){
+                return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+    if(njogada=="5"){
+        do{
+            if(dados[i]==5){
+                return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+    if(njogada=="6"){
+        do{
+            if(dados[i]==6){
+                return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+    if(njogada=="T"){
+        do{
+            if(dados[i]==3){
+                return 1; //certo
+            }
+
+        }while(i!=5);
+    }
+
 }
     
 }

9dc2f68c4a673f42aa0371ccda82d6d4003a60c8
Maria Isabela Silva Nunes <mariaisabelasn@gmail.com>
2023-09-20 14:38:00 -0300

    teste

--- UsaCampeonato.java
+++ UsaCampeonato.java
@@ -1,13 +1,36 @@
 package JogoGeneralTrabalho;
 import java.util.Scanner;
 
-public class UsaCampeonato {
-    public static void main(String []args){
-        Scanner teclado = new Scanner(System.in);
+public class Campeonato {
+    private Jogador[] players = new Jogador[5];
+       
+    public stat ic void main(String[] args){
 
-        System.out.println("..:: MENU ::..");
+    Scanner teclado = new Scanner (System.in);
+    int opcao = 0;
+
+    do{
+        System.out.println ("..:: Menu interativo ::..");
+        System.out.println ("1 - Ola mundo");
+        System.out.println ("2 - Ola POO" );
+        System.out.println ("3 - Sair");
+        System.out.print ("Entre com uma opcao: ");
+        opcao = teclado.nextInt( );
+
+        switch( opcao ){
+            case 1:
+                System.out.println ("Ola mundo");
+                break;
+            case 2:
+                System.out.println ("Ola POO");
+                break;
+            case 3:
+                System.out.println ("Saindo");
+                break;
+            default :
+                System.out.println ("Opcao invalida. Tente novamente");
+        }
+    }while(opcao != 3);
 
     }
-    
-}
-teste;
+ }

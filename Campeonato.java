29070843d70907f8b86ea530989d82d1d1736207
MacriFabiane <mariacristinafabiane@gmail.com>
2023-09-27 16:20:20 -0300

    fazendo merda

--- Campeonato.java
+++ Campeonato.java
@@ -2,6 +2,14 @@ package JogoGeneralTrabalho;
 
 public class Campeonato {
     private Jogador[] players = new Jogador[5];
+    int cont=0;
+
+    public void incluirjogador(nome player, tipoJogador biotipo){
+        if(cont != 5){
+            players[cont]=(player, biotipo);
+        }
+
+    }
        
     
  }
